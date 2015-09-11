564bfe4855dc246d8df12d5d746b7a02971f96b2
David Tran <david.tran@epitech.eu>
2015-09-11 02:57:53 +0200

    [web] 	- replaced sref by href for artists in home

--- web/js/artistController.js
+++ web/js/artistController.js
@@ -4,12 +4,11 @@
     angular.module('crowdModule')
         .controller('artistController', artistController);
 
-    artistController.$inject = ['$stateParams', '$http'];
+    artistController.$inject = ['$stateParams', '$http', '$rootScope'];
 
-    function artistController($stateParams, $http) {
+    function artistController($stateParams, $http, $rootScope) {
         var vm = this;
         vm.artname = $stateParams.name;
-
         $http.get('artiste.json').success(function(responses) {
           angular.forEach(responses, function(response, key) {
             if (response.name === vm.artname)
