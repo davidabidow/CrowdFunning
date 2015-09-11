90303d2d70236dc4419f1a8a44ab9bf07d1f9e5d
David Tran <david.tran@epitech.eu>
2015-09-11 12:40:55 +0200

    [WEB] 	- tabs fixed in homepage

--- web/js/homeController.js
+++ web/js/homeController.js
@@ -8,25 +8,16 @@
 
     function homeController($http, $rootScope) {
         var vm = this;
-        vm.active = 2;
-        vm.daily = true;
+        vm.active = 0;
         vm.triggerActive = triggerActive;
-        vm.addtoRoot = addtoRoot;
         $http.get('artiste.json').success(function(response) {
           vm.artists = response;
         });
         $http.get('ville.json').success(function(response) {
             vm.cities = response;
         });
-        function addtoRoot(name)
-        {
-          $rootScope.artistName = name;
-        }
         function triggerActive(option)
         {
-          if (vm.active === option)
-            vm.active = 2;
-          else
             vm.active = option;
         }
     }
